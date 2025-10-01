a17446a04406145cec12d5456edf1a50248a264b
David Tenni <davidctenni@gmail.com>
2025-10-01 20:41:32 +1000

    fixed so the old commit isnt being  used anymore

--- backend/order_service/app/main.py
+++ backend/order_service/app/main.py
@@ -17,7 +17,7 @@ from .db import Base, engine, get_db
 from .models import Order, OrderItem
 from .schemas import OrderCreate, OrderItemResponse, OrderResponse, OrderUpdate
 
-# test222222223
+# test2222222fuck23
 # --- Standard Logging Configuration ---
 logging.basicConfig(
     level=logging.INFO,
