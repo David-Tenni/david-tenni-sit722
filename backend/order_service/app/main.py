4fd94ba912d92a53e83361af473298a8d2353f80
David Tenni <davidctenni@gmail.com>
2025-10-01 20:59:15 +1000

    removed from main

--- backend/order_service/app/main.py
+++ backend/order_service/app/main.py
@@ -17,7 +17,7 @@ from .db import Base, engine, get_db
 from .models import Order, OrderItem
 from .schemas import OrderCreate, OrderItemResponse, OrderResponse, OrderUpdate
 
-# test2222222fuck23
+# test2222222fuck23asd
 # --- Standard Logging Configuration ---
 logging.basicConfig(
     level=logging.INFO,
