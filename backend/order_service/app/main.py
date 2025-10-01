d2b0122bb4fee7d2cd2de9f299de0310094551c4
David Tenni <davidctenni@gmail.com>
2025-10-01 17:09:10 +1000

    test

--- backend/order_service/app/main.py
+++ backend/order_service/app/main.py
@@ -17,7 +17,7 @@ from .db import Base, engine, get_db
 from .models import Order, OrderItem
 from .schemas import OrderCreate, OrderItemResponse, OrderResponse, OrderUpdate
 
-# test2222222
+# test22222222
 # --- Standard Logging Configuration ---
 logging.basicConfig(
     level=logging.INFO,
